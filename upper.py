046449a113f509be552b3a71f75b89304cb175a5
nitinssj3 <38137939+nitinssj3@users.noreply.github.com>
2018-07-13 00:55:35 +0530

    Add files via upload

---
+++ upper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 19:50:40 2018
+
+@author: Nitin
+"""
+
+import numpy as np
+ import pandas as pd
+ import matplotlib.pyplot as plt
+ 
+ dataset=pd.read_csv('Ads_CTR_Optimisation.csv')
